3b81b4bddb9be147961ae08a0da4e62111b1755e
sinnee <40656425+sinnee@users.noreply.github.com>
2022-11-10 00:40:32 +0900

    test husky

--- greeting.js
+++ greeting.js
@@ -1,6 +1,6 @@
-var greeting = function(rtm, channel){
-    console.log('인사를 합니다.');
-    rtm.sendMessage("Hello!", channel);
-}
+const greeting = function (rtm, channel) {
+  console.log('인사를 합니다.');
+  rtm.sendMessage('Hello!', channel);
+};
 
 module.exports = greeting;
