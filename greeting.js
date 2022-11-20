16ddcbc369b28657ec43eb863edf2f534b6db0a1
uha9218 <77494780+uha9218@users.noreply.github.com>
2022-11-20 21:13:03 +0900

    feat: Add greeting feature

--- greeting.js
+++ greeting.js
@@ -1,6 +1,20 @@
-var greeting = function(rtm, channel){
-    console.log('인사를 합니다.');
-    rtm.sendMessage("Hello!", channel);
+var greeting = function(rtm,channel){
+    console.log("greeting!");
+    let num = Math.floor(Math.random()*3)+1;
+    console.log(num);
+
+    switch(num){
+            case 1:
+                    rtm.sendMessage('Hello!',channel);
+                    break;
+            case 2:
+                    rtm.sendMessage('Hi!',channel);
+                    break;
+            case 3:
+                    rtm.sendMessage('What`s up!',channel);
+                    break;
+    }
 }
 
-module.exports = greeting;
+module.exports=greeting;
+
